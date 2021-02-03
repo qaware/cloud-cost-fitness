d2b18a89c5dc77647b9cfb66a2ea36f281799d7c
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2021-02-03 17:20:03 +0100

    Updated license header in source files.

--- src/test/groovy/de/qaware/cloud/aws/converter/ResponseConverterSpec.groovy
+++ src/test/groovy/de/qaware/cloud/aws/converter/ResponseConverterSpec.groovy
@@ -1,10 +1,17 @@
 /*
-   ________                __   ______           __     ______            _                      _
-  / ____/ /___  __  ______/ /  / ____/___  _____/ /_   / ____/___  ____ _(_)___  ___  ___  _____(_)___  ____ _
- / /   / / __ \/ / / / __  /  / /   / __ \/ ___/ __/  / __/ / __ \/ __ `/ / __ \/ _ \/ _ \/ ___/ / __ \/ __ `/
-/ /___/ / /_/ / /_/ / /_/ /  / /___/ /_/ (__  ) /_   / /___/ / / / /_/ / / / / /  __/  __/ /  / / / / / /_/ /
-\____/_/\____/\__,_/\__,_/   \____/\____/____/\__/  /_____/_/ /_/\__, /_/_/ /_/\___/\___/_/  /_/_/ /_/\__, /
-                                                                /____/                               /____/
+ *    Copyright (C) 2021 QAware GmbH
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 package de.qaware.cloud.aws.converter
 
